8bd6a4ad22595132b5e0272da113fa3bee2e8aa7
Sean Raven <sravendragon0@openmailbox.org>
2016-06-13 17:56:57 -0700

    switched to showing text

--- core/src/io/github/dragon0/mazes/MazeGame.scala
+++ core/src/io/github/dragon0/mazes/MazeGame.scala
@@ -2,17 +2,21 @@ package io.github.dragon0.mazes;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 class MazeGame extends Game {
     var batch:SpriteBatch = _
+    var font:BitmapFont = _
     
     override def create : Unit = {
         batch = new SpriteBatch()
+        font = new BitmapFont()
         setScreen(new SelectionScreen(this))
     }
 
     override def dispose : Unit = {
         batch.dispose()
+        font.dispose()
         super.dispose()
     }
 }
--- core/src/io/github/dragon0/mazes/SelectionScreen.scala
+++ core/src/io/github/dragon0/mazes/SelectionScreen.scala
@@ -3,22 +3,28 @@ package io.github.dragon0.mazes;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 
 class SelectionScreen(val game: MazeGame) extends ScreenAdapter {
 
-    var img:Texture = new Texture("badlogic.jpg");
+    var width = Gdx.graphics.getWidth()
+    var height = Gdx.graphics.getHeight()
 
     override def render(delta:Float) : Unit = {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.batch.draw(img, 0, 0);
+        game.font.draw(game.batch, "Hello World", width/2, height/2);
         game.batch.end();
     }
 
+    override def resize(width: Int, height: Int) : Unit = {
+        this.width = width
+        this.height = height
+    }
+
     override def dispose : Unit = {
-        img.dispose()
     }
 }
 
